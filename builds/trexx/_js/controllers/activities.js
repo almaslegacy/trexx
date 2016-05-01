c1555036bc9f69747a81e6941e61e3616cc796c4
Trey Ray <trey@almaslegacy.com>
2016-04-30 23:30:18 -0400

    readded activities.js

--- builds/trexx/_js/controllers/activities.js
+++ builds/trexx/_js/controllers/activities.js
@@ -1,5 +1,5 @@
-myApp.controller('ActivitiesController', ['$scope', '$http'], function ($scope, $http) {
+myApp.controller('ActivitiesController', ['$scope', '$http', function ($scope, $http) {
 	$http.get('data.json').success(function(data)	{
 		$scope.activities = data;
 		});
-}); //Activities Controller
+}]); //Activities Controller
