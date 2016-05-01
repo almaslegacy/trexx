35c69ff50f31c4eda7b3deed5d8a90e1d740e282
Trey Ray <trey@almaslegacy.com>
2016-05-01 03:28:14 -0400

    changed activities controller

--- builds/trexx/_js/controllers.js
+++ builds/trexx/_js/controllers.js
@@ -1,14 +1,14 @@
 var infoControllers = angular.module('infoControllers',[]);
 
 infoControllers.controller('ActivitiesController', ['$scope', '$http', function($scope, $http) {
-	$http.get('js/data.json').success(function(data) {
+	$http.get('data.json').success(function(data) {
 		$scope.activities = data;
 		$scope.activitiesOrder = 'Date';
 		});
 }]); //Activities Controller
 
 infoControllers.controller('ActivityController', ['$scope', '$http', 'routeParams', function($scope, $http) {
-	$http.get('js/data.json').success(function(data) {
+	$http.get('data.json').success(function(data) {
 		$scope.activities = data;
 		$scope.whichItem = $routeParams.itemId;
 		});
