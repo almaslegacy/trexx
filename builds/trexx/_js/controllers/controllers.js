563d7cc1de25fdcf1834e3b8258070e0550c4a9b
Trey Ray <trey@almaslegacy.com>
2016-04-30 23:35:45 -0400

    troubleshooting controllers.js

--- builds/trexx/_js/controllers/controllers.js
+++ builds/trexx/_js/controllers/controllers.js
@@ -1,7 +1,5 @@
-myApp.controller('MyController', function MyController($scope) {
-	$scope.info = {
-		'name' : 'Travis Ray',
-		'shortname' : 'Trayy',
-		'startdate' : 'July 2014'
-	}
-});
+myApp.controller('ActivitiesController', ['$scope', '$http', function ($scope, $http) {
+	$http.get('data.json').success(function(data)	{
+		$scope.activities = data;
+		});
+}]); //Activities Controller
