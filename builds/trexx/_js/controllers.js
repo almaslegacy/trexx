1b9ff01ba0b4b896dbe69cd91ea41a7d4b91e5d2
Trey Ray <trey@almaslegacy.com>
2016-05-01 02:45:44 -0400

    changed index.html back

--- builds/trexx/_js/controllers.js
+++ builds/trexx/_js/controllers.js
@@ -1,13 +1,13 @@
-var infoController = angular.module('infoController',[]);
+var infoControllers = angular.module('infoControllers',[]);
 
-infoController.controller('ActivitiesController', ['$scope', '$http', function($scope, $http) {
+infoControllers.controller('ActivitiesController', ['$scope', '$http', function($scope, $http) {
 	$http.get('js/data.json').success(function(data) {
 		$scope.activities = data;
 		$scope.activitiesOrder = 'Date';
 		});
 }]); //Activities Controller
 
-infoController.controller('ActivityController', ['$scope', '$http', 'routeParams', function($scope, $http) {
+infoControllers.controller('ActivityController', ['$scope', '$http', 'routeParams', function($scope, $http) {
 	$http.get('js/data.json').success(function(data) {
 		$scope.activities = data;
 		$scope.whichItem = $routeParams.itemId;
